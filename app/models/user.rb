a19aded4b1cacab90da1210c34e11abcf1733965
Nícolas <nicolasalmeida321@gmail.com>
2021-10-31 20:04:10 -0300

    Commit inicial

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+class User < ApplicationRecord
+  has_many :twitter_accounts
+  has_many :tweets
+
+  has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
+end

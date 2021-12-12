0fa5e2efa6312d8c9857c64a4f1629a902d1249a
nicollinoxx <nicolasalmeida321@gmail.com>
2021-12-12 11:49:10 -0300

    time

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ScheduledTweets02
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    Config.time_zone = 'Brasilia'
+    # Initialize configuration defaults for originally generated Rails version.
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
